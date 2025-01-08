8d895e16fb1d256b70734dc8eb932d22089a7d02
Barkat Ullah <barkatullah585464@gmail.com>
2025-01-08 10:10:44 +0530

    create project set up and file structure

---
+++ src/pages/admin/CreateAdmin.tsx
@@ -0,0 +1,9 @@
+const CreateAdmin = () => {
+  return (
+    <div>
+      <h2>CreateAdmin</h2>
+    </div>
+  );
+};
+
+export default CreateAdmin;
---
+++ src/pages/admin/CreateStudent.tsx
@@ -0,0 +1,9 @@
+const CreateStudent = () => {
+  return (
+    <div>
+      <h2>CreateStudent</h2>
+    </div>
+  );
+};
+
+export default CreateStudent;
